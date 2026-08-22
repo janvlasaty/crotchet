362c5f33731d58ca43478970d400a8fa206439d1
Jan Vlasatý <jan.vlasaty@hotmail.com>
2026-08-22 15:15:00 +0200

    feat: update version to 1.0.7 and change theme color to black in HTML and Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -37,8 +37,8 @@ export default defineConfig({
         scope: './',
         display: 'standalone',
         orientation: 'portrait',
-        background_color: '#0f0f0f',
-        theme_color: '#0f0f0f',
+        background_color: '#000000',
+        theme_color: '#000000',
         icons: [
           { src: 'icons/icon-192.png', sizes: '192x192', type: 'image/png' },
           { src: 'icons/icon-512.png', sizes: '512x512', type: 'image/png' },
